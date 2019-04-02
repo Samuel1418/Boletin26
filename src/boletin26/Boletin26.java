aab95483b06b9c1056fa000193b6a4c24734a01a
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-04-02 11:07:50 +0200

    Acabado

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin26;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Boletin26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Vent obx= new Vent();
+        obx.setVisible(true);
+    }
+    
+}
